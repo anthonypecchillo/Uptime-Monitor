c5d109de91397e4f8ac14957a04b4de6f1170239
anthonypecchillo <anthonypecchillo@gmail.com>
2018-08-07 21:45:20 -0400

    Add HTTP method parsing

--- index.js
+++ index.js
@@ -16,12 +16,14 @@ var server = http.createServer(function(req, res) {
   var path = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g, '');    // Trims off the slashes from both sides
                                                        // (Helps us to handle requests to /foo and /foo/ equally.)
+  // Get the HTTP method
+  var method = req.method.toUpperCase();
 
   // Send the response
   res.end('Hello World!\n');
 
   // Log the request path
-  console.log('Request received on path: ', trimmedPath);
+  console.log('Request received on path: ', trimmedPath, ' with method: ', method);
 });
 
 
