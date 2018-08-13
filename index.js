f68d597f66e49850170ec81bb916c97df5468b77
anthonypecchillo <anthonypecchillo@gmail.com>
2018-08-12 20:27:21 -0400

    Implement Read method

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ var fs = require('fs');
 var _data = require('./lib/data');
 
 
+// ------------------------------------------------------------
 // TESTING
+// ------------------------------------------------------------
 // @TODO delete this
-_data.create('test', 'newFile', {'foo': 'bar'}, function(err) {
-  console.log('This was the error: ', err);
+// _data.create('test', 'newFile', {'foo': 'bar'}, function(err) {
+//   console.log('This was the error: ', err);
+// });
+
+// @TODO delete this
+_data.read('test', 'newFile', function(err, data) {
+  console.log('This was the error: ', err, ' and this was the data: ', data);
 });
+// ------------------------------------------------------------
+
 
 // Instantiate the HTTP server
 var httpServer = http.createServer(function(req, res) {
